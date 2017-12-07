5b266b52a9d8523750778b959f49a3fef6a84ac0
Lewis Kim (seonghyeon Kim) <kshr2d2@gmail.com>
2017-12-07 20:53:34 +0900

    web server

--- server.py
+++ server.py
@@ -12,7 +12,45 @@ def page():
 
 sess = tf.InteractiveSession()
 
-def setum_net():
-    x = tf.placeholder(tf.float32, [None, 784])
-    x_image = tf.reshape(x, [-1, 28, 28, 1])
-    conv1 = tf.layers.conv2d
+def setup_net():
+    X = tf.placeholder(tf.float32)
+    X_img = tf.reshape(X, [-1, 28, 28, 1])
+
+    conv1 = tf.layers.conv2d(inputs=X_img, filters=32,
+                             padding='SAME', kernel_size=[3, 3],
+                             activation=tf.nn.relu,
+                             kernel_initializer=tf.contrib.layers.xavier_initializer())
+
+    pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2],
+                                    padding="SAME", strides=2)
+
+    conv2 = tf.layers.conv2d(inputs=pool1, filters=64, kernel_size=[3, 3],
+                             padding="SAME", activation=tf.nn.relu,
+                             kernel_initializer=tf.contrib.layers.xavier_initializer())
+
+    pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2],
+                                    padding="SAME", strides=2)
+
+    conv3 = tf.layers.conv2d(inputs=pool2, filters=128, kernel_size=[3, 3],
+                             padding="same", activation=tf.nn.relu,
+                             kernel_initializer=tf.contrib.layers.xavier_initializer())
+
+    pool3 = tf.layers.max_pooling2d(inputs=conv3, pool_size=[2, 2],
+                                    padding="same", strides=2)
+
+    flat = tf.reshape(pool3, [-1, 128 * 4 * 4])
+
+    dense4 = tf.layers.dense(inputs=flat,
+                             units=625, activation=tf.nn.relu,
+                             kernel_initializer=tf.contrib.layers.xavier_initializer())
+
+    logits = tf.layers.dense(inputs=dense4, units=26)
+
+    result = tf.argmax(logits, 1)
+
+    saver = tf.train.Saver()
+
+    with tf.Session() as sess:
+        # Restore variables from disk.
+        saver.restore(sess, "/tmp/model.ckpt")
+        print("Model restored.")
